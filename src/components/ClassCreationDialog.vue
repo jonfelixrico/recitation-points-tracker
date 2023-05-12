91190553fad443816c13af204348c46a7dd4ae03
Felix <jonfelixrico@gmail.com>
2023-05-13 04:37:04 +0800

    Polish the dialog

--- src/components/ClassCreationDialog.vue
+++ src/components/ClassCreationDialog.vue
@@ -1,20 +1,34 @@
 <template>
   <!-- notice dialogRef here -->
   <q-dialog ref="dialogRef" @hide="onDialogHide">
-    <q-form @submit.prevent="submitData">
-      <q-card class="q-dialog-plugin">
-        <q-input v-model="model.name">Name</q-input>
-        <q-input v-model.number="model.seatRows">Rows</q-input>
-        <q-input v-model.number="model.seatColumns">Columns</q-input>
+    <q-card class="q-dialog-plugin">
+      <q-form @submit.prevent="submitData">
+        <q-card-section class="text-h6"> Create Class </q-card-section>
+        <q-card-section class="column q-gutter-y-sm">
+          <q-input v-model="model.name" outlined label="Class name" />
+          <q-input
+            v-model.number="model.seatRows"
+            type="number"
+            outlined
+            label="Rows"
+          />
+
+          <q-input
+            v-model.number="model.seatColumns"
+            type="number"
+            outlined
+            label="Columns"
+          />
+        </q-card-section>
 
         <!-- TODO tags input -->
 
         <q-card-actions align="right">
-          <q-btn color="primary" label="OK" type="submit" />
-          <q-btn color="primary" label="Cancel" v-popup-close />
+          <q-btn flat label="Cancel" v-popup-close no-caps />
+          <q-btn color="primary" label="OK" type="submit" unelevated no-caps />
         </q-card-actions>
-      </q-card>
-    </q-form>
+      </q-form>
+    </q-card>
   </q-dialog>
 </template>
 
