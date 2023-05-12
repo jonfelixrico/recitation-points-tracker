16a277f6f78cf2ed71671a5123f50614fb8ddf67
Felix <jonfelixrico@gmail.com>
2023-05-13 00:50:14 +0800

    Create login page

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <q-btn @click="doAnonymousLogin">Log In</q-btn>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { useAuth } from 'src/composables/auth.composable'
+
+export default defineComponent({
+  setup() {
+    const { doAnonymousLogin } = useAuth()
+
+    return {
+      doAnonymousLogin,
+    }
+  },
+})
+</script>
