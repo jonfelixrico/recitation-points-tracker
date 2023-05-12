ba52955b092d993ff94cac4c4c16526df0690c9a
Felix <jonfelixrico@gmail.com>
2023-05-13 01:18:05 +0800

    Adjust the firebase config

--- src/composables/firebase.composable.ts
+++ src/composables/firebase.composable.ts
@@ -5,13 +5,12 @@ let app: FirebaseApp
 export function useFirebase() {
   if (!app) {
     const FIREBASE_CONFIG = {
-      apiKey: 'AIzaSyD3lfxbWXmctREdQkyekt6SXHQDN3yBfwE',
-      authDomain: 'recitation-points-tracker-test.firebaseapp.com',
-      projectId: 'recitation-points-tracker-test',
-      storageBucket: 'recitation-points-tracker-test.appspot.com',
-      messagingSenderId: '996746436257',
-      appId: '1:996746436257:web:6a3465006cbbf1c8d81ae0',
-      measurementId: 'G-9CX8PG3FVC',
+      apiKey: 'AIzaSyDHmM_wlcfQ8YMC97TzbDTrdN9nFpKbRIo',
+      authDomain: 'recitation-tracker.firebaseapp.com',
+      projectId: 'recitation-tracker',
+      storageBucket: 'recitation-tracker.appspot.com',
+      messagingSenderId: '382796561868',
+      appId: '1:382796561868:web:4689db3eedcd0e41bf3169',
     }
 
     app = initializeApp(FIREBASE_CONFIG)
