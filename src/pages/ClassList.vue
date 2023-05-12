c97ea2a1ee6d76c51e2e00e878ee30a94b461f1b
Felix <jonfelixrico@gmail.com>
2023-05-13 04:37:13 +0800

    Integrate the creation dialog into ClassList

--- src/pages/ClassList.vue
+++ src/pages/ClassList.vue
@@ -1,7 +1,9 @@
 <template>
   <q-page class="column">
     <div class="row justify-end q-pa-sm">
-      <q-btn no-caps color="primary" unelevated>Add Class</q-btn>
+      <q-btn no-caps color="primary" unelevated @click="showCreateDialog"
+        >Add Class</q-btn
+      >
     </div>
 
     <q-scroll-area v-if="classList.length" class="col">
@@ -19,14 +21,47 @@
 </template>
 
 <script lang="ts">
+import { useQuasar } from 'quasar'
+import ClassCreationDialog from 'src/components/ClassCreationDialog.vue'
 import { useClassesAPI } from 'src/composables/classes-api.composable'
 import { ClassEntity } from 'src/models/entities'
 import { defineComponent, ref, onBeforeMount } from 'vue'
+import { nanoid } from 'nanoid'
+import { useRouter } from 'vue-router'
+
+function useClassCreationDialog() {
+  const $q = useQuasar()
+  const { createClass } = useClassesAPI()
+  const router = useRouter()
+
+  return {
+    async showDialog() {
+      $q.dialog({
+        component: ClassCreationDialog,
+      }).onOk(async (data: Omit<ClassEntity, 'id'>) => {
+        const classId = nanoid()
+
+        await createClass({
+          ...data,
+          id: classId,
+        })
+
+        await router.push({
+          name: 'classDetails',
+          params: {
+            classId,
+          },
+        })
+      })
+    },
+  }
+}
 
 export default defineComponent({
   setup() {
-    const { getClassList } = useClassesAPI()
+    const { showDialog: showCreateDialog } = useClassCreationDialog()
 
+    const { getClassList } = useClassesAPI()
     const classList = ref<ClassEntity[]>([])
 
     onBeforeMount(async () => {
@@ -39,6 +74,7 @@ export default defineComponent({
 
     return {
       classList,
+      showCreateDialog,
     }
   },
 })
