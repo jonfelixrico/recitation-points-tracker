51478491ee5b9f6810b214d0af2d06f08d51df10
Felix <jonfelixrico@gmail.com>
2023-05-13 04:03:31 +0800

    Implement some code into the ClassLIst component

--- src/pages/ClassList.vue
+++ src/pages/ClassList.vue
@@ -1,3 +1,37 @@
 <template>
-  <q-page> Class list </q-page>
+  <q-page>
+    <q-list>
+      <q-item v-for="item in classList" :key="item.id">
+        <q-item-section>
+          {{ item.name }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-page>
 </template>
+
+<script lang="ts">
+import { useClassesAPI } from 'src/composables/classes-api.composable'
+import { ClassEntity } from 'src/models/entities'
+import { defineComponent, ref, onBeforeMount } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const { getClassList } = useClassesAPI()
+
+    const classList = ref<ClassEntity[]>([])
+
+    onBeforeMount(async () => {
+      try {
+        classList.value = await getClassList()
+      } catch (e) {
+        console.error(e)
+      }
+    })
+
+    return {
+      classList,
+    }
+  },
+})
+</script>
