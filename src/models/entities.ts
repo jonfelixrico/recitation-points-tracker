9e282e56e8dd3b96beafdff1f7c21555a73433aa
Felix <jonfelixrico@gmail.com>
2023-05-13 03:37:11 +0800

    Adjust the entities

--- src/models/entities.ts
+++ src/models/entities.ts
@@ -1,6 +1,17 @@
 export interface ClassEntity {
   id: string
   name: string
-  seatColumns?: number
-  seatRows?: number
+  seatColumns: number
+  seatRows: number
+  tags: string[]
+}
+
+export interface StudentEntity {
+  id: string
+
+  firstName: string
+  lastName: string
+
+  seatColumn: string
+  seatRow: string
 }
