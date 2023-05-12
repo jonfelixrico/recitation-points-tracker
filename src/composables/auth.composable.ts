04fcf8b60daf67e9d05bfbf749a1288f0e77e049
Felix <jonfelixrico@gmail.com>
2023-05-13 02:04:29 +0800

    Use app

--- src/composables/auth.composable.ts
+++ src/composables/auth.composable.ts
@@ -4,25 +4,26 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth'
 import { useFirebase } from './firebase.composable'
+import { FirebaseApp } from 'firebase/app'
 
 const EMULATOR_HOST = process.env.FIREBASE_AUTH_EMULATOR_HOST
 if (EMULATOR_HOST) {
   console.debug('Using %s for emulator host', EMULATOR_HOST)
 }
 
-function getAuthInstance() {
+function getAuthInstance(app: FirebaseApp) {
   if (!EMULATOR_HOST) {
-    const app = useFirebase()
     return getAuth(app)
   }
 
-  const auth = getAuth()
+  const auth = getAuth(app)
   connectAuthEmulator(auth, EMULATOR_HOST)
   return auth
 }
 
 export function useAuth() {
-  const auth = getAuthInstance()
+  const app = useFirebase()
+  const auth = getAuthInstance(app)
 
   return {
     loginViaEmailAndPassword(email: string, password: string) {
