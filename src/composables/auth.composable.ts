d0506bf3152514c7d1a95bb6776411a65ee0a324
Felix <jonfelixrico@gmail.com>
2023-05-13 01:26:05 +0800

    Adjust the auth composable

--- src/composables/auth.composable.ts
+++ src/composables/auth.composable.ts
@@ -1,9 +1,12 @@
 import { getAuth, signInAnonymously } from 'firebase/auth'
+import { useFirebase } from './firebase.composable'
 
 export function useAuth() {
+  const app = useFirebase()
+  const auth = getAuth(app)
+
   return {
     doAnonymousLogin() {
-      const auth = getAuth()
       return signInAnonymously(auth)
     },
   }
