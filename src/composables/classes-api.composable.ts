2f9ac1a9006f9d41975493d80a2fc7d5ba51f00a
Felix <jonfelixrico@gmail.com>
2023-05-13 03:34:02 +0800

    Implement some API methods

---
+++ src/composables/classes-api.composable.ts
@@ -0,0 +1,25 @@
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
+import { useFirestore } from './firestore.composable'
+import { useSessionUserId } from './session-user.composable'
+import { ClassEntity } from 'src/models/class.entity'
+
+export function useClassesAPI() {
+  const { firestore } = useFirestore()
+  const uid = useSessionUserId()
+
+  async function getClassList() {
+    const docs = await getDocs(collection(firestore, `users/${uid}/classes`))
+    const output: ClassEntity[] = []
+    docs.forEach((doc) => {
+      // TODO use validation or something
+      output.push(doc.data() as ClassEntity)
+    })
+  }
+
+  async function getClass(id: string) {
+    const docRef = doc(firestore, `users/${uid}/classes`, id)
+    const docSnap = await getDoc(docRef)
+    // do something about this
+    return docSnap.data() as ClassEntity
+  }
+}
---
+++ src/models/class.entity.ts
@@ -0,0 +1,6 @@
+export interface ClassEntity {
+  id: string
+  name: string
+  seatColumns?: number
+  seatRows?: number
+}
