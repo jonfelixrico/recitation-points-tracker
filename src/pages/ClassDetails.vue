1226f45cb7c51783047401b8d6e64c2fdc16093a
Felix <jonfelixrico@gmail.com>
2023-05-13 04:14:02 +0800

    Implement class details functionality

--- src/pages/ClassDetails.vue
+++ src/pages/ClassDetails.vue
@@ -1,3 +1,36 @@
 <template>
-  <q-page> Class details</q-page>
+  <q-page>{{ classData }}</q-page>
 </template>
+
+<script lang="ts">
+import { useClassesAPI } from 'src/composables/classes-api.composable'
+import { ClassEntity } from 'src/models/entities'
+import { defineComponent, onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+export default defineComponent({
+  async beforeRouteEnter(to) {
+    const { getClass } = useClassesAPI()
+    try {
+      await getClass(String(to.params.classId))
+    } catch (e) {
+      return false
+    }
+  },
+
+  setup() {
+    const { getClass } = useClassesAPI()
+    const route = useRoute()
+
+    const classData = ref<ClassEntity | null>(null)
+
+    onMounted(async () => {
+      classData.value = await getClass(String(route.params.classId))
+    })
+
+    return {
+      classData,
+    }
+  },
+})
+</script>
