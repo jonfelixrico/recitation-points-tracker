1a27e919e7d69d23f843c46d973d3da0848cea32
Felix <jonfelixrico@gmail.com>
2023-05-16 00:31:32 +0800

    Add failing tests

--- test/cypress/component/components/class-details/AddStudentsForm.cy.ts
+++ test/cypress/component/components/class-details/AddStudentsForm.cy.ts
@@ -1,11 +1,37 @@
 import AddStudentsInput from 'src/components/class-details/AddStudentsInput.vue'
+import { StudentEntity } from 'src/models/entities'
 
 describe('AddStudentsInput', () => {
-  it('should display data in the model', () => {
+  it('should display data in the model - empty', () => {
     cy.mount(AddStudentsInput, {
       props: {
-        modelValue: [],
+        modelValue: [] as Omit<StudentEntity, 'id'>[],
       },
     })
+
+    cy.dataCy('student').should('not.exist')
+  })
+
+  it('should display data in the model - populated', () => {
+    cy.mount(AddStudentsInput, {
+      props: {
+        modelValue: [
+          {
+            firstName: 'Student',
+            lastName: 'Student 1',
+            seatColumn: 1,
+            seatRow: 1,
+          },
+          {
+            firstName: 'Student',
+            lastName: 'Student 2',
+            seatColumn: 1,
+            seatRow: 1,
+          },
+        ] as Omit<StudentEntity, 'id'>[],
+      },
+    })
+
+    cy.dataCy('student').should('have.length', 2)
   })
 })
