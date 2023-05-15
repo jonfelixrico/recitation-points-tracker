6f9855543321b183f727edbc2e09e591951cc75a
Felix <jonfelixrico@gmail.com>
2023-05-16 00:33:38 +0800

    Satisfy the tests

--- src/components/class-details/AddStudentsInput.vue
+++ src/components/class-details/AddStudentsInput.vue
@@ -1,6 +1,19 @@
-<template>Stub</template>
-<script lang="ts">
-import { defineComponent } from 'vue'
+<template>
+  <div v-for="(student, index) of modelValue" :key="index" data-cy="student">
+    {{ student }}
+  </div>
+</template>
 
-export default defineComponent({})
+<script lang="ts">
+import { StudentEntity } from 'src/models/entities'
+import { PropType, defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    modelValue: {
+      required: true,
+      type: Array as PropType<StudentEntity[]>,
+    },
+  },
+})
 </script>
