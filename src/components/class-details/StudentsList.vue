3eff77203ad212dd7fdd40c1cd94eaff057aa225
Felix <jonfelixrico@gmail.com>
2023-05-15 23:47:09 +0800

    Create StudentsList component

---
+++ src/components/class-details/StudentsList.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-list v-if="students.length" separator>
+    <q-item v-for="student of students" :key="student.id">
+      <q-item-section>
+        {{ student }}
+      </q-item-section>
+    </q-item>
+  </q-list>
+
+  <div v-else class="flex flex-center empty-list-height">
+    <div class="text-h6">No students to show</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { StudentEntity } from 'src/models/entities'
+import { PropType, defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    students: {
+      required: true,
+      type: Array as PropType<StudentEntity[]>,
+    },
+  },
+})
+</script>
+
+<style lang="scss">
+// TODO make this more responsive
+.empty-list-height {
+  height: 300px;
+}
+</style>
