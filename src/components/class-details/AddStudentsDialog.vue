31c7cfe36e1ee478b1d24fba2c9a45d918cc5f17
Felix <jonfelixrico@gmail.com>
2023-05-16 00:21:06 +0800

    Create stub for AddStudentDialog

---
+++ src/components/class-details/AddStudentsDialog.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- notice dialogRef here -->
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin">
+      <!-- stub -->
+    </q-card>
+  </q-dialog>
+</template>
+
+<script lang="ts">
+import { useDialogPluginComponent } from 'quasar'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  emits: [...useDialogPluginComponent.emits],
+
+  setup() {
+    const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent()
+
+    return {
+      dialogRef,
+      onDialogHide,
+
+      submitData() {
+        onDialogOK()
+      },
+    }
+  },
+})
+</script>
