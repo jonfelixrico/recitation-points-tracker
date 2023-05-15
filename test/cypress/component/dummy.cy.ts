74a245bf5059b3128e7149b6ba76cee29ec6a5fe
Felix <jonfelixrico@gmail.com>
2023-05-16 00:17:55 +0800

    Remove dummy tests

--- test/cypress/component/dummy.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('remove if tests are added', () => {
-  /*
-   * Remove this test file once you add actual tests.
-   * This is just here to prevent Cypress from throwing errors because there are no tests yet.
-   */
-})
