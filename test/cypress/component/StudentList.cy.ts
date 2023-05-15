f5f29513852fb40040d42a69f04f74a67097e5c1
Felix <jonfelixrico@gmail.com>
2023-05-16 00:17:41 +0800

    Add failing tests

---
+++ test/cypress/component/StudentList.cy.ts
@@ -0,0 +1,34 @@
+import StudentList from 'src/components/class-details/StudentList.vue'
+import { StudentEntity } from 'src/models/entities'
+
+describe('StudentList', () => {
+  it('should show a list if there are students', () => {
+    cy.mount(StudentList, {
+      props: {
+        students: [
+          {
+            id: 'test',
+            firstName: 'Student',
+            lastName: '1',
+            seatColumn: 1,
+            seatRow: 1,
+          },
+        ] as StudentEntity[],
+      },
+    })
+
+    cy.dataCy('student-content').should('exist')
+    cy.dataCy('empty-notice').should('not.exist')
+  })
+
+  it('should show a notice if there are no students', () => {
+    cy.mount(StudentList, {
+      props: {
+        students: [] as StudentEntity[],
+      },
+    })
+
+    cy.dataCy('empty-notice').should('exist')
+    cy.dataCy('student-content').should('not.exist')
+  })
+})
