dd6038e905982750769b02ae68719879024fb9dc
Felix <jonfelixrico@gmail.com>
2023-05-18 00:06:58 +0800

    Integrate to dialog

--- src/components/class-details/AddStudentsDialog.vue
+++ src/components/class-details/AddStudentsDialog.vue
@@ -2,28 +2,43 @@
   <!-- notice dialogRef here -->
   <q-dialog ref="dialogRef" @hide="onDialogHide">
     <q-card class="q-dialog-plugin">
-      <!-- stub -->
+      <q-card-section>
+        <AddStudentsForm v-model="studentsList" />
+      </q-card-section>
     </q-card>
   </q-dialog>
 </template>
 
 <script lang="ts">
 import { useDialogPluginComponent } from 'quasar'
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
+import AddStudentsForm from './AddStudentsForm.vue'
+
+// TODO move to proper types
+interface PartialStudent {
+  firstName: string
+  lastName: string
+}
 
 export default defineComponent({
+  components: { AddStudentsForm },
+
   emits: [...useDialogPluginComponent.emits],
 
   setup() {
     const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent()
+
+    const studentsList = ref<PartialStudent[]>([])
 
     return {
       dialogRef,
       onDialogHide,
 
       submitData() {
-        onDialogOK()
+        onDialogOK(studentsList)
       },
+
+      studentsList,
     }
   },
 })
