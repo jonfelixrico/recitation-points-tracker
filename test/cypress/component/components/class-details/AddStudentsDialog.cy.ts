5907b68aa82ec7a19712e236aa5aca7446f0dec8
Felix <jonfelixrico@gmail.com>
2023-05-18 00:13:17 +0800

    Add failing tests

---
+++ test/cypress/component/components/class-details/AddStudentsDialog.cy.ts
@@ -0,0 +1,28 @@
+import DialogWrapper from 'app/test/cypress/wrappers/DialogWrapper.vue'
+import AddStudentsDialog from 'src/components/class-details/AddStudentsDialog.vue'
+
+describe('AddStudentsDialog', () => {
+  it('should integrate with the form', () => {
+    cy.mount(DialogWrapper, {
+      props: {
+        component: AddStudentsDialog,
+      },
+    })
+
+    cy.withinDialog((el) => {
+      cy.wrap(el)
+        .dataCy('form')
+        .dataCy('input')
+        .dataCy('last-name')
+        .type('First name')
+
+      cy.wrap(el)
+        .dataCy('form')
+        .dataCy('input')
+        .dataCy('last-name')
+        .type('Last name')
+
+      cy.wrap(el).dataCy('form').dataCy('input').dataCy('submit').click()
+    })
+  })
+})
