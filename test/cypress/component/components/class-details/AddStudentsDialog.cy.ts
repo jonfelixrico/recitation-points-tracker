a1cd669a24d5a181efe14585052e5476a31db5c2
Felix <jonfelixrico@gmail.com>
2023-05-18 00:22:27 +0800

    Adjust the tests

--- test/cypress/component/components/class-details/AddStudentsDialog.cy.ts
+++ test/cypress/component/components/class-details/AddStudentsDialog.cy.ts
@@ -2,27 +2,34 @@ import DialogWrapper from 'app/test/cypress/wrappers/DialogWrapper.vue'
 import AddStudentsDialog from 'src/components/class-details/AddStudentsDialog.vue'
 
 describe('AddStudentsDialog', () => {
-  it('should integrate with the form', () => {
+  it('should integrate properly with the form component', () => {
     cy.mount(DialogWrapper, {
       props: {
         component: AddStudentsDialog,
       },
     })
 
-    cy.withinDialog((el) => {
-      cy.wrap(el)
-        .dataCy('form')
-        .dataCy('input')
-        .dataCy('first-name')
-        .type('First name')
+    cy.withinDialog({
+      persistent: true,
+      fn: (el) => {
+        for (let i = 1; i <= 5; i++) {
+          cy.wrap(el)
+            .dataCy('form')
+            .dataCy('input')
+            .dataCy('first-name')
+            .type(`FN ${i}`)
 
-      cy.wrap(el)
-        .dataCy('form')
-        .dataCy('input')
-        .dataCy('last-name')
-        .type('Last name')
+          cy.wrap(el)
+            .dataCy('form')
+            .dataCy('input')
+            .dataCy('last-name')
+            .type(`LN ${i}`)
 
-      cy.wrap(el).dataCy('form').dataCy('input').dataCy('submit').click()
+          cy.wrap(el).dataCy('form').dataCy('input').dataCy('submit').click()
+        }
+
+        cy.wrap(el).dataCy('form').dataCy('student').should('have.length', 5)
+      },
     })
   })
 })
