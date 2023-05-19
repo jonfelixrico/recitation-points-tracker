9f326669e47227efb132c26e32bb19f6e7125541
Felix <jonfelixrico@gmail.com>
2023-05-20 03:41:52 +0800

    Adjust i18n

--- src/i18n/en-US/index.ts
+++ src/i18n/en-US/index.ts
@@ -1,9 +1,11 @@
 import common from './common'
 import classes from './classes'
 import session from './session'
+import mainLayout from './main-layout'
 
 export default {
   classes,
   common,
   session,
+  mainLayout,
 }
