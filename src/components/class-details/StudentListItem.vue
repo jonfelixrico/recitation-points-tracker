d05e61bf5169e422fdc527cde65c063644076b83
Felix <jonfelixrico@gmail.com>
2023-05-20 02:23:23 +0800

    Create StudentListItem component

---
+++ src/components/class-details/StudentListItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-item>
+    <q-item-section>
+      <i18n-t keypath="common.nameFormat" tag="span" class="white-space-pre">
+        <template #firstName>
+          <span>{{ student.firstName }}</span>
+        </template>
+
+        <template #lastName>
+          <span class="font-weight-bold">{{
+            student.lastName.toUpperCase()
+          }}</span>
+        </template>
+      </i18n-t>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import { StudentEntity } from 'src/models/entities'
+import { PropType, defineProps } from 'vue'
+
+defineProps({
+  student: {
+    required: true,
+    type: Object as PropType<StudentEntity>,
+  },
+})
+</script>
