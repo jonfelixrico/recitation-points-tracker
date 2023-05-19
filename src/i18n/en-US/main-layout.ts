941b247938c79f6a21749f1f582225ef96b70689
Felix <jonfelixrico@gmail.com>
2023-05-20 03:48:25 +0800

    Adjust i18n

--- src/i18n/en-US/main-layout.ts
+++ src/i18n/en-US/main-layout.ts
@@ -2,4 +2,12 @@ export default {
   toolbar: {
     logOut: 'Log out',
   },
+
+  dialogs: {
+    logOut: {
+      title: 'Log Out',
+      message: 'Are you sure that you want to log out?',
+      okLabel: 'Yes, log me out',
+    },
+  },
 }
