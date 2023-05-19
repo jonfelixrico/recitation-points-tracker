5d1acfbe46f3a058e1742627941e4976ff9c8f07
Felix <jonfelixrico@gmail.com>
2023-05-20 02:33:21 +0800

    Remove unused components

--- src/components/class-details/StudentList.vue
+++ src/components/class-details/StudentList.vue
@@ -40,7 +40,6 @@
 import { StudentEntity } from 'src/models/entities'
 import { PropType } from 'vue'
 import { useI18n } from 'vue-i18n'
-import StudentListItem from './StudentListItem.vue'
 
 defineProps({
   students: {
