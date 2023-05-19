bd06d7e0f490854c4495e981976d5bd348ae0a64
Felix <jonfelixrico@gmail.com>
2023-05-20 01:21:39 +0800

    Create failing tests

--- test/cypress/component/components/class-details/AddStudentsDialog.cy.ts
+++ test/cypress/component/components/class-details/AddStudentsDialog.cy.ts
@@ -36,4 +36,42 @@ describe('AddStudentsDialog', () => {
       },
     })
   })
+
+  it('should handle removing', () => {
+    cy.mount(DialogWrapper, {
+      props: {
+        component: AddStudentsDialog,
+      },
+    })
+
+    cy.withinDialog({
+      fn: (el) => {
+        cy.wrap(el).dataCy('empty-message').should('exist')
+
+        const ITEM_COUNT = 3
+
+        for (let i = 1; i <= ITEM_COUNT; i++) {
+          cy.wrap(el).dataCy('input').dataCy('first-name').type(`FN ${i}`)
+          cy.wrap(el).dataCy('input').dataCy('last-name').type(`LN ${i}`)
+          cy.wrap(el).dataCy('form').dataCy('input').dataCy('submit').click()
+        }
+
+        cy.wrap(el).dataCy('item').should('have.length', ITEM_COUNT)
+        cy.wrap(el).dataCy('empty-message').should('not.exist')
+
+        for (let i = ITEM_COUNT; i >= 1; i--) {
+          cy.wrap(el)
+            .dataCy('item')
+            .eq(i - 1)
+            .dataCy('delete-button')
+            .click()
+        }
+
+        cy.wrap(el).dataCy('item').should('not.exist')
+        cy.wrap(el).dataCy('empty-message').should('exist')
+
+        cy.wrap(el).dataCy('submit-button').should('be.disabled')
+      },
+    })
+  })
 })
