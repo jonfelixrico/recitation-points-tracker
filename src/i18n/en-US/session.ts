3f9175d99d09b7ac07b25860e703b297fb6c434e
Felix <jonfelixrico@gmail.com>
2023-05-20 03:35:38 +0800

    Add i18n

--- src/i18n/en-US/session.ts
+++ src/i18n/en-US/session.ts
@@ -1,4 +1,14 @@
 export default {
   logIn: 'Log in',
   logOut: 'Log out',
+
+  fields: {
+    username: {
+      label: 'Username',
+    },
+
+    password: {
+      label: 'Password',
+    },
+  },
 }
