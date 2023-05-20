27482ea2198f8832b2343f6ce18e4035ff11a5d5
Felix <jonfelixrico@gmail.com>
2023-05-20 17:17:03 +0800

    Satisfy the tests

--- src/components/SeatingArrangement.vue
+++ src/components/SeatingArrangement.vue
@@ -1,3 +1,36 @@
-<template>Stub</template>
+<template>
+  <div class="row no-wrap q-gutter-x-xs">
+    <div
+      v-for="columnNo in columnCount"
+      :key="columnNo"
+      class="column q-gutter-y-xs"
+      data-cy="column"
+    >
+      <div v-for="rowNo in rowCount" :key="rowNo" data-cy="seat" class="seat" />
+    </div>
+  </div>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  rowCount: {
+    required: true,
+    type: Number,
+  },
+  columnCount: {
+    required: true,
+    type: Number,
+  },
+})
+</script>
+
+<style scoped lang="scss">
+.seat {
+  $size: 10px;
+
+  width: $size;
+  height: $size;
+
+  background-color: $grey;
+}
+</style>
