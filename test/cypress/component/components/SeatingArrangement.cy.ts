24f38325c236f344fdc1e6d79e47853320f4a520
Felix <jonfelixrico@gmail.com>
2023-05-20 17:16:57 +0800

    Adjust the tests

--- test/cypress/component/components/SeatingArrangement.cy.ts
+++ test/cypress/component/components/SeatingArrangement.cy.ts
@@ -1,14 +1,15 @@
 import SeatingArrangement from 'src/components/SeatingArrangement.vue'
 
 describe('SeatingArrangement', () => {
-  it('should have the correct seat count', () => {
+  it('should have the correct arrangement displayed', () => {
     cy.mount(SeatingArrangement, {
       props: {
-        columnCount: 5,
+        columnCount: 8,
         rowCount: 5,
       },
     })
 
-    cy.dataCy('seat').should('have.length', 25)
+    cy.dataCy('seat').should('have.length', 40)
+    cy.dataCy('column').should('have.length', 8)
   })
 })
