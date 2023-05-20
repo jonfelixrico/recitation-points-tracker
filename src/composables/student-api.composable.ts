98bd9ed265c9100a0297fec6e382277861a06822
Felix <jonfelixrico@gmail.com>
2023-05-20 15:57:48 +0800

    Create delete method

--- src/composables/student-api.composable.ts
+++ src/composables/student-api.composable.ts
@@ -1,4 +1,10 @@
-import { collection, doc, getDocs, writeBatch } from 'firebase/firestore'
+import {
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  writeBatch,
+} from 'firebase/firestore'
 import { useFirestore } from './firestore.composable'
 import { useSessionUserId } from './session-user.composable'
 import { StudentEntity } from 'src/models/entities'
@@ -51,6 +57,16 @@ export function useStudentAPI() {
       await batch.commit()
 
       return insertedStudents
+    },
+
+    async deletStudent(classId: string, studentId: string) {
+      const docRef = doc(
+        firestore,
+        `users/${uid}/classes/${classId}/students`,
+        studentId
+      )
+
+      await deleteDoc(docRef)
     },
   }
 }
