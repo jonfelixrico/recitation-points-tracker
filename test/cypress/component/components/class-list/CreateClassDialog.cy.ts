cd0f3b2b57a138cb845ee7cb3ecf63a5525f4a81
Felix <jonfelixrico@gmail.com>
2023-05-20 21:31:47 +0800

    Add failing tests

--- test/cypress/component/components/class-list/CreateClassDialog.cy.ts
+++ test/cypress/component/components/class-list/CreateClassDialog.cy.ts
@@ -2,11 +2,52 @@ import DialogWrapper from 'app/test/cypress/wrappers/DialogWrapper.vue'
 import CreateClassDialog from 'components/class-list/CreateClassDialogV2.vue'
 
 describe('CreateClassDialog', () => {
-  it('todo add message', () => {
+  it('should handle adding and removing of columns', () => {
     cy.mount(DialogWrapper, {
       props: {
         component: CreateClassDialog,
       },
+    })
+
+    cy.withinDialog((el) => {
+      cy.wrap(el).dataCy('column-item').should('have.length', 0)
+
+      cy.wrap(el)
+        .dataCy('add-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 1)
+        })
+
+      cy.wrap(el)
+        .dataCy('add-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 2)
+        })
+
+      cy.wrap(el)
+        .dataCy('add-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 3)
+        })
+
+      cy.wrap(el)
+        .dataCy('subract-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 2)
+        })
+
+      cy.wrap(el)
+        .dataCy('subract-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 1)
+        })
+
+      cy.wrap(el).dataCy('cancel-button').click()
     })
   })
 })
