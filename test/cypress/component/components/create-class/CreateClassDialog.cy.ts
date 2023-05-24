2e78d8551b7ec8afe5f65e36af3ea3cd05b433a4
Felix <jonfelixrico@gmail.com>
2023-05-25 00:13:57 +0800

    Create failing tests

--- test/cypress/component/components/create-class/CreateClassDialog.cy.ts
+++ test/cypress/component/components/create-class/CreateClassDialog.cy.ts
@@ -2,6 +2,53 @@ import DialogWrapper from 'app/test/cypress/wrappers/DialogWrapper.vue'
 import CreateClassDialog from 'components/create-class/CreateClassDialog.vue'
 
 describe('CreateClassDialog', () => {
+  it('should emit the proper seat arrangement', () => {
+    const okSpy = cy.spy()
+    cy.mount(DialogWrapper, {
+      props: {
+        component: CreateClassDialog,
+        onOk: okSpy,
+      },
+    })
+
+    cy.withinDialog((el) => {
+      cy.wrap(el).dataCy('column-item').should('have.length', 0)
+
+      cy.wrap(el)
+        .dataCy('add-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 1)
+          cy.wrap(el).dataCy('column-item').eq(0).dataCy('input').type('5')
+        })
+
+      cy.wrap(el)
+        .dataCy('add-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 2)
+          cy.wrap(el).dataCy('column-item').eq(1).dataCy('input').type('6')
+        })
+
+      cy.wrap(el)
+        .dataCy('add-button')
+        .click()
+        .then(() => {
+          cy.wrap(el).dataCy('column-item').should('have.length', 3)
+          cy.wrap(el).dataCy('column-item').eq(2).dataCy('input').type('7')
+        })
+
+      cy.wrap(el)
+        .dataCy('submit-button')
+        .click()
+        .then(() => {
+          expect(okSpy).to.have.been.called.calledWith({
+            seatArrangement: [5, 6, 7],
+          })
+        })
+    })
+  })
+
   it('should handle adding and removing of columns, and input should stay in place', () => {
     cy.mount(DialogWrapper, {
       props: {
