6531f04ca453c2a30c3abff132d06adbf305a90d
Felix <jonfelixrico@gmail.com>
2023-05-25 22:55:41 +0800

    Create stub for the class seating arrangement

---
+++ src/components/class-details/ClassSeatingArrangement.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="row">
+    <!-- TODO adjust side panel UI -->
+    <div class="side-panel flex flex-center">
+      <!-- TODO i18nize this -->
+      No students
+    </div>
+    <q-scroll-area class="col">
+      <!-- TODO add seating grid component -->
+    </q-scroll-area>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+
+defineProps({
+  columns: {
+    type: Array as PropType<number[]>,
+    required: true,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.side-panel {
+  width: 200px;
+}
+</style>
