5969456290b16ef6d023444fbbe2e47a059cf7d1
Felix <jonfelixrico@gmail.com>
2023-05-25 22:47:47 +0800

    Make SeatingArrangement use SeatingGrid

--- src/components/seating/SeatingArrangement.vue
+++ src/components/seating/SeatingArrangement.vue
@@ -1,43 +1,26 @@
 <template>
-  <div
-    class="row no-wrap q-gutter-x-xs"
-    :style="{
-      '--tile-size': `${tileSize}px`,
-    }"
-  >
-    <div
-      v-for="(seatCount, colNo) of arrangement"
-      :key="colNo"
-      class="column q-gutter-y-xs"
-      data-cy="column"
-    >
-      <template v-for="rowNo in seatCount" :key="rowNo">
-        <div
-          v-if="!indexedOccupants[colNo]?.[rowNo]"
-          data-cy="seat"
-          data-empty
-          class="seat"
-          :data-row-no="rowNo"
-          :data-col-no="colNo"
-        />
+  <SeatingGrid :columns="arrangement" :tile-size="tileSize">
+    <template v-slot="{ colNo, rowNo }">
+      <div
+        v-if="!indexedOccupants[colNo]?.[rowNo]"
+        data-empty
+        class="bg-grey"
+      />
 
-        <div
-          v-else
-          data-cy="seat"
-          class="seat occupied"
-          :data-occupant-id="indexedOccupants[colNo][rowNo].id"
-          data-occupied
-          :data-row-no="rowNo"
-          :data-col-no="colNo"
-        />
-      </template>
-    </div>
-  </div>
+      <div
+        v-else
+        class="bg-green"
+        :data-occupant-id="indexedOccupants[colNo][rowNo].id"
+        data-occupied
+      />
+    </template>
+  </SeatingGrid>
 </template>
 
 <script setup lang="ts">
 import { PropType, computed } from 'vue'
 import { SeatOccupant } from './seating-types'
+import SeatingGrid from './SeatingGrid.vue'
 
 const props = defineProps({
   arrangement: {
@@ -50,10 +33,7 @@ const props = defineProps({
     default: () => [],
   },
 
-  tileSize: {
-    type: Number,
-    default: () => 20,
-  },
+  tileSize: Number,
 })
 
 const indexedOccupants = computed(() => {
@@ -70,16 +50,3 @@ const indexedOccupants = computed(() => {
   return map
 })
 </script>
-
-<style scoped lang="scss">
-.seat {
-  width: var(--tile-size);
-  height: var(--tile-size);
-
-  background-color: $grey;
-
-  &.occupied {
-    background-color: $green;
-  }
-}
-</style>
