2c19ab0006a854c5c8df1dc1eadd2daf673554e5
Felix <jonfelixrico@gmail.com>
2023-05-27 00:02:25 +0800

    Create dialog component

---
+++ src/components/class-seat-plan/ClassSeatPlanEditDialog.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin">
+      <!--
+        ...content
+        ... use q-card-section for it?
+      -->
+
+      <!-- buttons example -->
+      <q-card-actions align="right">
+        <q-btn color="primary" label="OK" @click="onDialogOK" />
+        <q-btn color="primary" label="Cancel" v-close-popup />
+      </q-card-actions>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup lang="ts">
+import { useDialogPluginComponent } from 'quasar'
+
+const props = defineProps({
+  // ...your custom props
+})
+
+defineEmits([
+  // REQUIRED; need to specify some events that your
+  // component will emit through useDialogPluginComponent()
+  ...useDialogPluginComponent.emits,
+])
+
+const { dialogRef, onDialogHide, onDialogOK } = useDialogPluginComponent()
+</script>
