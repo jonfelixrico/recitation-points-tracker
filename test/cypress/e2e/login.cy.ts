e80d4d19d54773ed84c0aeb7fc3ef257042a054d
Felix <jonfelixrico@gmail.com>
2023-05-26 23:35:13 +0800

    Create login test

---
+++ test/cypress/e2e/login.cy.ts
@@ -0,0 +1,14 @@
+describe('login', () => {
+  it('should log in', () => {
+    cy.visit('/')
+    cy.dataCy('username').type('user1@jrico.dev')
+    cy.dataCy('password').type('p@ssw0rd')
+    cy.dataCy('submit')
+      .click()
+      .then(() => {
+        cy.location().should((location) => {
+          expect(location.pathname).does.not.contain('login')
+        })
+      })
+  })
+})
