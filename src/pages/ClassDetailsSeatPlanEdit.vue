8f070f436a838e36805a6c5d6582f809a56c50a9
Felix <jonfelixrico@gmail.com>
2023-05-26 23:58:54 +0800

    Create edit seat plan page

---
+++ src/pages/ClassDetailsSeatPlanEdit.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page>
+    <div class="page-width q-mx-auto column q-gutter-y-md">
+      <div class="row items-center q-gutter-x-lg">
+        <q-btn icon="arrow_back" round flat dense @click="$router.back()" />
+        <div class="text-h4">Edit Seat Plan</div>
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
