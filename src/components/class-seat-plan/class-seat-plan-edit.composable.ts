6fc9dc7329e36a25dc797c4e16c4ce70005764d3
Felix <jonfelixrico@gmail.com>
2023-05-27 00:09:18 +0800

    Create composable

---
+++ src/components/class-seat-plan/class-seat-plan-edit.composable.ts
@@ -0,0 +1,22 @@
+import { useQuasar } from 'quasar'
+import { SeatingArrangement, StudentEntity } from 'src/models/entities'
+import ClassSeatPlanEditDialog from './ClassSeatPlanEditDialog.vue'
+
+export function useClassSeatPlanEdit() {
+  const { dialog } = useQuasar()
+
+  return {
+    openEditDialog(
+      seatingArrangement: SeatingArrangement,
+      students: StudentEntity[]
+    ) {
+      return dialog({
+        component: ClassSeatPlanEditDialog,
+        componentProps: {
+          students,
+          seatingArrangement,
+        },
+      })
+    },
+  }
+}
