00aa13523d7dbdfde0d15b8ba023d9c75c8ee8ff
Felix <jonfelixrico@gmail.com>
2023-05-27 00:44:42 +0800

    Create the visualizer for the seat plan

---
+++ src/components/class-seat-plan/SeatPlanVisualizer.vue
@@ -0,0 +1,21 @@
+<template>
+  <SeatingGrid :columns="modelValue" />
+</template>
+
+<script setup lang="ts">
+import SeatingGrid from 'component/seating/SeatingGrid.vue'
+import { SeatingArrangement, StudentEntity } from 'src/models/entities'
+import { PropType } from 'vue'
+
+defineProps({
+  modelValue: {
+    required: true,
+    type: Object as PropType<SeatingArrangement>,
+  },
+
+  students: {
+    required: true,
+    type: Array as PropType<StudentEntity[]>,
+  },
+})
+</script>
