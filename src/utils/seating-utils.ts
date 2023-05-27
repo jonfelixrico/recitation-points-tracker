4f963bedebca4e940960450a232db4c939221450
Felix <jonfelixrico@gmail.com>
2023-05-28 00:27:29 +0800

    Add comments, do proper return value

--- src/utils/seating-utils.ts
+++ src/utils/seating-utils.ts
@@ -1,11 +1,24 @@
-export function computeStartingSeatCountPerColumn(columns: number[]) {
-  const startingSeatCount: number[] = []
+/**
+ * Helper for computing the seat index.
+ *
+ * HOW TO USE:
+ * Determine column index.
+ * Determine row index within the column.
+ * seat index will be seatCountPerColumn[columnIndex] + rowIndex
+ *
+ * @param columns
+ * @returns Index in `columns` is directly mapped with the index in the return value.
+ */
+export function computeStartingSeatCountPerColumn(columns: number[]): number[] {
+  const startingSeatCountPerColumn: number[] = []
 
   for (let index = 0; index < columns.length; index++) {
     const totalSeatCountOfPreviousColumns = columns
       .slice(0, index)
       .reduce((acc, val) => acc + val, 0)
 
-    startingSeatCount.push(totalSeatCountOfPreviousColumns)
+    startingSeatCountPerColumn.push(totalSeatCountOfPreviousColumns)
   }
+
+  return startingSeatCountPerColumn
 }
