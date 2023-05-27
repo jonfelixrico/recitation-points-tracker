b66c998797e2e40eb1e97f0b99e7b45eb9ebc499
Felix <jonfelixrico@gmail.com>
2023-05-28 02:42:35 +0800

    Implement saving functionality

--- src/components/class-seat-plan/class-seat-plan-edit-dialog.composable.ts
+++ src/components/class-seat-plan/class-seat-plan-edit-dialog.composable.ts
@@ -1,15 +1,40 @@
 import { useQuasar } from 'quasar'
-import { SeatingArrangement, StudentEntity } from 'src/models/entities'
+import {
+  ClassEntity,
+  SeatingArrangement,
+  StudentEntity,
+} from 'src/models/entities'
 import ClassSeatPlanEditDialog from './ClassSeatPlanEditDialog.vue'
+import { useClassesAPI } from 'src/composables/classes-api.composable'
+
+type Occupants = SeatingArrangement['occupants']
 
 export function useClassSeatPlanEdit() {
-  const { dialog } = useQuasar()
+  const { dialog, loading } = useQuasar()
+  const { setSeatOccupants } = useClassesAPI()
+
+  async function uploadSeatChanges(id: string, occupants: Occupants) {
+    loading.show()
+    try {
+      await setSeatOccupants(id, occupants)
+      // TODO show some sort of notification
+    } catch (e) {
+      console.error(e)
+      // TODO show some sort of error
+    } finally {
+      loading.hide()
+    }
+  }
 
   return {
     openEditDialog(
-      seatingArrangement: SeatingArrangement,
+      { seatingArrangement, id }: ClassEntity,
       students: StudentEntity[]
     ) {
+      if (!seatingArrangement) {
+        throw new Error('No seating arrangement')
+      }
+
       return dialog({
         component: ClassSeatPlanEditDialog,
         componentProps: {
@@ -17,6 +42,8 @@ export function useClassSeatPlanEdit() {
           columns: seatingArrangement.columns,
           occupants: seatingArrangement.occupants,
         },
+      }).onOk((data: Occupants) => {
+        uploadSeatChanges(id, data)
       })
     },
   }
