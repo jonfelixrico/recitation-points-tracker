afd9b7bd09e39dcc552826e1ef715caa56de219a
Felix <jonfelixrico@gmail.com>
2023-05-30 01:35:59 +0800

    Remove stray logging

--- src/utils/class-model-utils.ts
+++ src/utils/class-model-utils.ts
@@ -55,7 +55,6 @@ export async function validateAndCovertClassEntityBody(
     excludeExtraneousValues: true,
   })
 
-  console.log(converted)
   await validateOrReject(converted)
 
   return converted
