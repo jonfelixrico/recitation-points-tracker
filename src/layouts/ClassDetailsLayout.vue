b4fbf8ae454a1c0ef619ef263a90861b5da0b7d9
Felix <jonfelixrico@gmail.com>
2023-05-31 20:15:11 +0800

    Add the UI for switching

--- src/layouts/ClassDetailsLayout.vue
+++ src/layouts/ClassDetailsLayout.vue
@@ -4,6 +4,39 @@
       <div class="row items-center q-gutter-x-lg">
         <q-btn icon="arrow_back" round flat dense @click="$router.back()" />
         <div class="text-h4">Test</div>
+
+        <div class="col row justify-end">
+          <q-tabs
+            active-color="primary"
+            indicator-color="transparent"
+            dense
+            class="text-weight-medium"
+            content-class="q-gutter-x-sm row"
+          >
+            <q-route-tab
+              :to="{ name: 'classDetails' }"
+              no-caps
+              active-class="bg-primary text-white"
+              class="rounded-borders"
+              exact
+              replace
+            >
+              <!-- TODO i18nize -->
+              Overview
+            </q-route-tab>
+
+            <q-route-tab
+              :to="{ name: 'classRecitations' }"
+              no-caps
+              active-class="bg-primary text-white"
+              class="rounded-borders"
+              exact
+              replace
+            >
+              Recitations
+            </q-route-tab>
+          </q-tabs>
+        </div>
       </div>
       <router-view />
     </div>
