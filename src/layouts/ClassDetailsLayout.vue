0544638c6f1b06eff9b0065a09d68f7297ba46c6
Felix <jonfelixrico@gmail.com>
2023-05-31 19:57:53 +0800

    Adjust the class details layout

--- src/layouts/ClassDetailsLayout.vue
+++ src/layouts/ClassDetailsLayout.vue
@@ -1,9 +1,21 @@
 <template>
-  <q-page>
-    <router-view />
+  <q-page class="q-pt-md relative-position">
+    <div class="page-width q-mx-auto column q-gutter-y-md">
+      <div class="row items-center q-gutter-x-lg">
+        <q-btn icon="arrow_back" round flat dense @click="$router.back()" />
+        <div class="text-h4">Test</div>
+      </div>
+      <router-view />
+    </div>
   </q-page>
 </template>
 
-<script setup lang="ts">
-// TODO add something
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  beforeRouteEnter() {
+    console.log('hi')
+  },
+})
 </script>
