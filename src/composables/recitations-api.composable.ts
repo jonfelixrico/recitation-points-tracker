4f49ecf232d1e0d48b2ac0f0906572d4df1b3393
Felix <jonfelixrico@gmail.com>
2023-06-03 00:19:34 +0800

    Use converters

--- src/composables/recitations-api.composable.ts
+++ src/composables/recitations-api.composable.ts
@@ -1,49 +1,80 @@
 import { RecitationEntity } from 'src/models/entities'
 import { useFirestore } from './firestore.composable'
-import { collection, doc, getDocs, setDoc } from 'firebase/firestore'
+import {
+  DocumentReference,
+  Firestore,
+  FirestoreDataConverter,
+  collection,
+  doc,
+  getDocs,
+  setDoc,
+} from 'firebase/firestore'
 import { useSessionUserId } from './session-user.composable'
 import { nanoid } from 'nanoid'
+
+function useRecitationsDataConverter(
+  firestore: Firestore,
+  uid: string
+): FirestoreDataConverter<RecitationEntity> {
+  return {
+    fromFirestore(snapshot) {
+      const data = snapshot.data()
+      const classRef = data.classRef as DocumentReference
+      return {
+        id: snapshot.id,
+        classId: classRef.id,
+        name: data.name,
+        tags: data.tags,
+      }
+    },
+
+    toFirestore({ classId, name, tags }) {
+      return {
+        classRef: doc(firestore, `userse/${uid}/classes/${classId}`),
+        name,
+        tags,
+      }
+    },
+  }
+}
 
 export function useRecitationsAPI() {
   const { firestore } = useFirestore()
   const uid = useSessionUserId()
+
+  const converter = useRecitationsDataConverter(firestore, String(uid))
 
   return {
     async createRecitation(
       body: Omit<RecitationEntity, 'id'>
     ): Promise<RecitationEntity> {
       const id = nanoid()
-      const { classId, ...others } = body
+      const newRecitation = {
+        ...body,
+        id,
+      }
 
       const recitationRef = doc(
         firestore,
-        `users/${uid}/classes/${classId}/recitations`,
+        `users/${uid}/classes/${body.classId}/recitations`,
         id
-      )
+      ).withConverter(converter)
+      await setDoc(recitationRef, body)
 
-      await setDoc(recitationRef, {
-        ...others,
-        classRef: doc(firestore, `users/${uid}/classes`, classId),
-      })
-
-      return {
-        id,
-        classId,
-        ...others,
-      }
+      return newRecitation
     },
 
     async getRecitationList(classId: string) {
       const recitations: RecitationEntity[] = []
 
-      const results = await getDocs(
-        collection(firestore, `users/${uid}/classes/${classId}/recitations`)
-      )
+      const sourceCollection = collection(
+        firestore,
+        `users/${uid}/classes/${classId}/recitations`
+      ).withConverter(converter)
+      const results = await getDocs(sourceCollection)
+
       results.forEach((doc) => {
-        recitations.push({
-          ...doc.data(),
-          id: doc.id,
-        } as RecitationEntity)
+        recitations.push(doc.data())
       })
 
       return recitations
