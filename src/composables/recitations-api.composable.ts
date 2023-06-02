70bcb83f3807dcbb93d78ea0e890aa6c9839744e
Felix <jonfelixrico@gmail.com>
2023-06-02 23:29:07 +0800

    Create initial code for recitations API

---
+++ src/composables/recitations-api.composable.ts
@@ -0,0 +1,29 @@
+import { RecitationEntity } from 'src/models/entities'
+import { useFirestore } from './firestore.composable'
+import { doc, setDoc } from 'firebase/firestore'
+import { useSessionUserId } from './session-user.composable'
+import { nanoid } from 'nanoid'
+
+export function useRecitationApi() {
+  const { firestore } = useFirestore()
+  const uid = useSessionUserId()
+
+  return {
+    async createRecitation(body: Omit<RecitationEntity, 'id'>) {
+      const id = nanoid()
+      const { classId, ...others } = body
+
+      const ref = doc(
+        firestore,
+        'users',
+        String(uid),
+        'classes',
+        classId,
+        'recitations',
+        id
+      )
+
+      await setDoc(ref, others)
+    },
+  }
+}
