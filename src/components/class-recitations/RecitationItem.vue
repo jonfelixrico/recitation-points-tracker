2d2e69b682cf0a935fb6a146a312902083f0bdec
Felix <jonfelixrico@gmail.com>
2023-06-08 00:57:33 +0800

    Create list component

---
+++ src/components/class-recitations/RecitationItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-item>
+    <q-item-section>
+      <div class="text-h6">{{ recitation.name }}</div>
+      <div>{{ recitation.recitationDt }}</div>
+    </q-item-section>
+    <q-item-section side>
+      <!-- TODO i18nize this -->
+      <q-btn>View Recitation</q-btn>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script setup lang="ts">
+import { RecitationEntity } from 'src/models/entities'
+import { PropType } from 'vue'
+
+defineProps({
+  recitation: {
+    required: true,
+    type: Object as PropType<RecitationEntity>,
+  },
+})
+</script>
