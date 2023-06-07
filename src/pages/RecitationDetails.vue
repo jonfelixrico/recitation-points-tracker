8cb92015dfe7c5f9c436442851baa89f2bfbe69d
Felix <jonfelixrico@gmail.com>
2023-06-08 01:03:24 +0800

    Create stub of recitation details

---
+++ src/pages/RecitationDetails.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page> Test </q-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  beforeRouteEnter() {
+    // TODO do something
+  },
+})
+</script>
