cc95ce26d3baddb4ba99e63513ceab286269f828
Felix <jonfelixrico@gmail.com>
2023-06-11 22:06:47 +0800

    Apply rename and comments

--- src/composables/class-data-composable.ts
+++ src/composables/class-data-composable.ts
@@ -5,7 +5,7 @@ import { Ref, ref, watch } from 'vue'
 
 type ClassData = ClassEntity & { students: StudentEntity[] }
 
-export function useRecitationData(classId: Ref<string>) {
+export function useClassData(classId: Ref<string>) {
   const { getClass } = useClassesAPI()
   const { getStudentList } = useStudentAPI()
 
@@ -14,13 +14,15 @@ export function useRecitationData(classId: Ref<string>) {
   watch(
     [classId],
     async ([classId]) => {
-      const getClassOutput = (await getClass(classId)) as ClassEntity
+      const getClassOutput = (await getClass(classId)) as ClassEntity // trust as not null since null checking should be done at the route guard level
       const getStudentsOutput = await getStudentList(classId)
 
       data.value = {
         ...getClassOutput,
         students: getStudentsOutput,
       }
+
+      // TODO add logging and error handling
     },
     {
       immediate: true,
