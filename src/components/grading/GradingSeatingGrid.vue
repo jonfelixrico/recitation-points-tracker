b177b21660bf6275620ea0529a8ef3c87df02ad5
Felix <jonfelixrico@gmail.com>
2023-06-14 23:44:32 +0800

    Create component for the seating grid for grading

---
+++ src/components/grading/GradingSeatingGrid.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- TODO add something -->
+  </div>
+</template>
+
+<script setup lang="ts">
+import {
+  RecitedStudentsEntity,
+  SeatingArrangement,
+  StudentEntity,
+} from 'src/models/entities'
+import { PropType } from 'vue'
+import { RecitedStudentsEntityMap } from './grading-types'
+
+defineProps({
+  seatingArrangement: {
+    required: true,
+    type: Object as PropType<SeatingArrangement>,
+  },
+
+  studentList: {
+    required: true,
+    type: Array as PropType<StudentEntity[]>,
+  },
+
+  recitedStudents: {
+    required: true,
+    type: Object as PropType<RecitedStudentsEntityMap>,
+  },
+})
+
+defineEmits<{
+  (e: 'student-update', value: RecitedStudentsEntity): void
+}>()
+</script>
